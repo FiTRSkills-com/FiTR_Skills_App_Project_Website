70bbef90656498fde4e2b6c53997f750d76aa197
gjb2054 <gjb2054@rit.edu>
2022-09-08 18:10:18 -0400

    Updated Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { projectDescription } from '../content';
 
 function Home() {
     return (
-        <div>
-            <h2 class='header-text'>FiTR Skills App</h2>
-        </div>
+        <>
+            <div>
+                <h2 class='header-text'>FiTR Skills App</h2>
+            </div>
+        {
+            projectDescription.map( (paragraph) => (
+                <p className='bodyText'>{paragraph}</p>
+            ))
+        }
+        </>
     );
 }
   
---
+++ src/content.js
@@ -0,0 +1,23 @@
+export const projectDescription = [
+    "FiTR Skills is a talent acquisition and skills training platform",
+
+    " Skills First Acquistion and Matching",
+];
+
+export const projectLinks = [
+    {
+        link: "https://github.com/FiTRSkills-com",
+        text: "GitHub",
+    },
+    {
+        link: "http://www.fitrskills.com/",
+        text: "FITRSkills",
+    },
+];
+
+export const projectPlan = [
+    "The project description can be found on the home page",
+    "‎", // empty line character
+
+    "Our Project Plan goes here",
+];
