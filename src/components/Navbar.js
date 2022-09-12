3cf927c8b4508f5740cd274d4219e45ce47ec218
gjb2054 <gjb2054@rit.edu>
2022-09-12 16:05:29 -0400

    Additional Components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,11 @@ import './Navbar.css';
 function Navbar() {
     return (
         <div class = "navbar">
-            <Link to="/" class="navbar-item"> Home </Link>
+            <Link to="/" class="navbar-item">Home</Link>
             <Link to="/time" class="navbar-item">Time Tracking</Link>
             <Link to="/four-up" class="navbar-item">FourUp Charts</Link>
-            <Link to="/" class="navbar-item">Project Plan</Link>
+            <Link to="/" class="navbar-item">Artifacts</Link>
             <Link to="/" class="navbar-item">Metrics</Link>
-            <Link to="/" class="navbar-item">Domain Model</Link>
             <Link to="/" class="navbar-item">Action Items 2.0</Link>
         </div>
     );
